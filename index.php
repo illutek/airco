215e8301e4483c0d34511622ca31f4b663d4465b
Vandenborne Stefan <stefan@illutek.be>
2018-05-18 08:55:33 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Airco Dirk</title>
+</head>
+<body>
+
+<div class="container">
+    <div class="container--center">
+        <header class="header">
+            <div class="logo">
+                <img src="images/logo.png" alt="Airco Dirk Noben">
+            </div>
+            <div class="nav">
+                <ul>
+                    <li><a href='#'>Home</a></li>
+                    <li><a href='#'>Info</a></li>
+                    <li><a href='#'>Contact</a></li>
+                </ul>
+            </div>
+        </header>
+
+        <section class="wrapper">
+            <div class="content">
+                <p>Voor een correcte werking en professioneel onderhoud van uw autoairconditioning 
+                moet u bij Dirk Noben zijn. Of het nu gaat om schadereparatie, bijvulling van airco-installaties 
+                in auto’s, landbouwmachines, vrachtwagens e.d., opsporen van lekkages en geurhinder, Dirk Noben, 
+                specialist in onderhoud en service van autoairco, met meer dan 30 jaar ervaring, helpt u gegarandeerd verder!</p>
+            </div>
+            <div class="sidebar">
+                Noben Dirk <br>
+                Beverststraat 29b <br>
+                3600 Genk <br>
+                Telefoon 089/351501 <br>
+                GSM 0479/408953 <br>
+                BE 638.623.155 <br>
+            </div>
+        </section>
+    </div>    
+</div>
+
+</body>
+</html>
