494eab40f3c64a34330b2f8e816ebb659df28582
Vandenborne Stefan <stefan@illutek.be>
2018-05-18 15:18:25 +0200

    css-grid and responsief first step

--- index.php
+++ index.php
@@ -10,36 +10,39 @@
 <body>
 
 <div class="container">
-    <div class="container--center">
-        <header class="header">
-            <div class="logo">
-                <img src="images/logo.png" alt="Airco Dirk Noben">
+    <div class="container--center">        
+
+            <div class="header__logo">
+                <img class="header__logo__img" src="images/logo.png" alt="Airco Dirk Noben">
             </div>
+
             <div class="nav">
-                <ul>
+                <ul class="nav__list">
                     <li><a href='#'>Home</a></li>
                     <li><a href='#'>Info</a></li>
                     <li><a href='#'>Contact</a></li>
                 </ul>
             </div>
-        </header>
-
-        <section class="wrapper">
+      
+            <div class="sidebar">
+                <p>
+                    Noben Dirk <br>
+                    Beverststraat 29b <br>
+                    3600 Genk <br>
+                    Telefoon 089/351501 <br>
+                    GSM 0479/408953 <br>
+                    BE 638.623.155 <br>
+                </p>
+                
+            </div>
+        
             <div class="content">
                 <p>Voor een correcte werking en professioneel onderhoud van uw autoairconditioning 
                 moet u bij Dirk Noben zijn. Of het nu gaat om schadereparatie, bijvulling van airco-installaties 
                 in auto’s, landbouwmachines, vrachtwagens e.d., opsporen van lekkages en geurhinder, Dirk Noben, 
                 specialist in onderhoud en service van autoairco, met meer dan 30 jaar ervaring, helpt u gegarandeerd verder!</p>
-            </div>
-            <div class="sidebar">
-                Noben Dirk <br>
-                Beverststraat 29b <br>
-                3600 Genk <br>
-                Telefoon 089/351501 <br>
-                GSM 0479/408953 <br>
-                BE 638.623.155 <br>
-            </div>
-        </section>
+            </div>            
+
     </div>    
 </div>
 
