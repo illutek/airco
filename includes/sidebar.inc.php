fb0e6166967a66f753d24d4fb881a8ba0bfd4c1e
Vandenborne Stefan <stefan@illutek.be>
2018-05-20 21:51:29 +0200

    sidebar__contact to flex

--- includes/sidebar.inc.php
+++ includes/sidebar.inc.php
@@ -5,27 +5,32 @@
 ?>
 
 <div class="sidebar__contact">
+
+  <div class="sidebar__contact__address-region">
     <p>
-        Beverststraat 29b <br>
-        3600 Genk<br>
-        BE 638.623.155
-
-        <div class="sidebar__contact__phone--mobile">
-          <a href="tel: +3289351501" class="btn btn--white"><i class="fas fa-phone"></i> 089/351501</a>
-        </div>
-        <div class="sidebar__contact__phone">
-          <i class="fas fa-phone"></i> 089/351501
-        </div>
-        
-
-        <div class="sidebar__contact__phone--mobile">
-          <a href="tel: +32479408953" class="btn btn--white"> <i class="fas fa-mobile-alt"></i> 0479/408953</a>
-        </div>
-        <div class="sidebar__contact__phone">
-          <i class="fas fa-mobile-alt"></i> 0479/408953
-        </div>
-        
+      Beverststraat 29b <br>
+      3600 Genk<br>
+      BE 638.623.155
     </p>
+  </div>
+  
+  <div class="sidebar__contact__phone-region">
+    <div class="sidebar__contact__phone--mobile">
+      <a href="tel: +3289351501" class="btn btn--white"><i class="fas fa-phone"></i> 089/351501</a>
+    </div>
+    <div class="sidebar__contact__phone">
+      <i class="fas fa-phone"></i> 089/351501
+    </div>
+
+    <div class="sidebar__contact__phone--mobile">
+      <a href="tel: +32479408953" class="btn btn--white"> <i class="fas fa-mobile-alt"></i> 0479/408953</a>
+    </div>
+    <div class="sidebar__contact__phone">
+      <i class="fas fa-mobile-alt"></i> 0479/408953
+    </div>      
+  </div>
+    
+  
 </div>
 
 <div class="sidebar__footer">
