bfec5a2a06a7db10c9f4cb8000c7d4286db43513
Vandenborne Stefan <stefan@illutek.be>
2018-05-21 11:35:52 +0200

    title updated

--- info.php
+++ info.php
@@ -14,7 +14,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" type="text/css" href="css/style.css">
-    <title>Airco Dirk</title>
+    <title>Airco-Dirk autoairco-aircopomp-aircocompressor-airco onderhoud</title>
 </head>
 <body>
 
